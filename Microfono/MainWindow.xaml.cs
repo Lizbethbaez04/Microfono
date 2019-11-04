b5593840dc29e2622c858ff31844ce297aa37ba6
alumno <alumno@E321>
2019-11-04 11:50:30 -0700

    bytes y bits

--- Microfono/MainWindow.xaml.cs
+++ Microfono/MainWindow.xaml.cs
@@ -51,7 +51,54 @@ namespace Microfono
 
         private void WaveIn_DataAvailable(object sender, WaveInEventArgs e)
         {
-            throw new NotImplementedException();
+            byte[] buffer = e.Buffer;
+            //Cuantos bytes se guardaron
+            int bytesGrabados = e.BytesRecorded;
+
+            //bytes = 8 bits; 2 bytes es una muestra
+            int numMuestras = bytesGrabados / 2;
+
+            int exponente = 1;
+            int numBits = 0;
+
+            do
+            {
+                exponente++;
+                numBits = (int)Math.Pow(2, exponente);
+            } while (numBits < numMuestras);
+            exponente -= 1;
+            numBits = (int) Math.Pow(2, exponente);
+            Complex[] muestrasComplejas = new Complex[numBits];
+
+            for(int i = 0; i < bytesGrabados; i += 2)
+            {
+                //Variable de 16 bits
+                // <<Asigna los bits que se pone despues de eso
+                short muestra = (short)(buffer[i + 1] << 8 | buffer[i]);
+                float muestra32bits = (float)muestra / 32768.0f;
+                if(i/2 < numBits)
+                {
+                    muestrasComplejas[i / 2].X = muestra32bits;
+                }               
+            }
+            //Nos permite regresar al dominio del tiempo
+            FastFourierTransform.FFT(true, exponente, muestrasComplejas);
+            float[] valoresAbsolutos = new float[muestrasComplejas.Length];
+
+            for(int i=0; i < muestrasComplejas.Length; i++)
+            {
+                valoresAbsolutos[i] = (float)Math.Sqrt((muestrasComplejas[i].X * muestrasComplejas[i].X) + (muestrasComplejas[i].Y * muestrasComplejas[i].Y));
+            }
+
+            int indiceValorMaximo = valoresAbsolutos.ToList().IndexOf(valoresAbsolutos.Max());
+            float frecuenciaFundamental = (float)(indiceValorMaximo * formato.SampleRate) / (float)valoresAbsolutos.Length;
+
+            lblHertz.Text = frecuenciaFundamental.ToString("n") + "Hz";
+        }
+
+        private void BtnDetener_Click(object sender, RoutedEventArgs e)
+        {
+            waveIn.StopRecording();
         }
     }
 }
